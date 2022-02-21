717741db01ffa9ab9ae375d1490279235f8d157f
Ljuba Rankovic <ljubar@gmail.com>
2022-02-21 11:26:19 +0100

    Export english content

--- src/Factory/InsajderNinjsFactory.php
+++ src/Factory/InsajderNinjsFactory.php
@@ -19,228 +19,72 @@ class InsajderNinjsFactory extends NinjsFactory
         10 => [        
             'sections' => [
                 10 => [
-                    'name' => 'Tema',
-                    'code' => 'tema',
-                ],
-                12 => [
-                    'name' => 'Pitanje dana',
-                    'code' => 'pitanjedana',
+                    'name' => 'In Focus',
+                    'code' => 'focus',
                 ],
                 15 => [
-                    'name' => 'Najvažnije vesti',
-                    'code' => 'najvaznijevesti',
+                    'name' => 'News',
+                    'code' => 'news',
                 ],
-                23 => [
-                    'name' => 'Istraživačke priče',
-                    'code' => 'istrazivackeprice',
-                ],
-                25 => [
-                    'name' => 'Društvo',
-                    'code' => 'drustvo',
-                ],
-                26 => [
-                    'name' => 'Podkast',
-                    'code' => 'podkast',
+                17 => [
+                    'name' => 'Positive stories',
+                    'code' => 'positive',
                 ],
                 30 => [
-                    'name' => 'Stav redakcije',
-                    'code' => 'stav',
+                    'name' => 'We recommend',
+                    'code' => 'recommended',
                 ],
-                97 => [
-                    'name' => 'Pregled nedelje',
-                    'code' => 'preglednedelje',
-                ],
-                98 => [
-                    'name' => 'Insajder debata',
-                    'code' => 'debata',
+                50 => [
+                    'name' => 'About Insajder',
+                    'code' => 'about',
                 ],
                 99 => [
-                    'name' => 'Insajder bez ograničenja',
+                    'name' => 'Insajder without limits',
                     'code' => 'bezogranicenja',
-                ],
-                413 => [
-                    'name' => 'Politika kao biznis, država kao partijski plen',
-                    'code' => 'partijskadrzava',
-                ],
-                412 => [
-                    'name' => 'Epidemija tajni',
-                    'code' => 'epidemijatajni',
-                ],
-                414 => [
-                    'name' => 'Lokalni šerifi',
-                    'code' => 'lokalniserifi',
-                ],
-                411 => [
-                    'name' => 'Insajder: Tačka',
-                    'code' => 'mediji',
-                ],
-                415 => [
-                    'name' => 'Dovršen plan',
-                    'code' => 'dovrsenplan',
-                ],
-                410 => [
-                    'name' => 'Prodaja pod zavetom ćutanja',
-                    'code' => 'prodajagalenika',
-                ],
-                400 => [
-                    'name' => 'Prodaja',
-                    'code' => 'prodaja',
-                ],
-                390 => [
-                    'name' => 'Srpsko-arapska posla',
-                    'code' => 'srpskoarapskaposla',
-                ],
-                380 => [
-                    'name' => 'Insajder specijal',
-                    'code' => 'specijal',
-                ],
-                370 => [
-                    'name' => 'Prvenstvo u prevari',
-                    'code' => 'prvenstvouprevari',
-                ],
-                240 => [
-                    'name' => 'Patriotska pljačka, nastavak',
-                    'code' => 'patriotskapljackanastavak',
-                ],
-                140 => [
-                    'name' => 'Energetski (ne)sporazum',
-                    'code' => 'energetski',
-                ],
-                200 => [
-                    'name' => 'Rudnik dugova',
-                    'code' => 'rudnikdugova',
-                ],
-                210 => [
-                    'name' => 'Pravila pljačke',
-                    'code' => 'pravilapljacke',
-                ],
-                100 => [
-                    'name' => 'Patriotska pljačka',
-                    'code' => 'patriotskapljacka',
-                ],
-                360 => [
-                    'name' => 'Kupoprodaja zdravlja',
-                    'code' => 'vakcine',
-                ],
-                150 => [
-                    'name' => 'Prevara veka',
-                    'code' => 'prevaraveka',
-                ],
-                220 => [
-                    'name' => 'Insajder o Insajderu',
-                    'code' => 'insoins',
-                ],
-                160 => [
-                    'name' => 'Nasilje uz blagoslov',
-                    'code' => 'nasilje',
-                ],
-                230 => [
-                    'name' => '(Ne)moć države',
-                    'code' => 'nemocdrzave',
                 ],
                 110 => [
                     'name' => 'Službena (zlo)upotreba',
                     'code' => 'sluzbena',
                 ],
+                150 => [
+                    'name' => 'Prevara veka',
+                    'code' => 'prevaraveka',
+                ],
+                170 => [
+                    'name' => 'Službena tajna',
+                    'code' => 'sluzbenatajna',
+                ],
                 190 => [
                     'name' => 'Pravila igre',
                     'code' => 'pravilaigre',
                 ],
-                260 => [
-                    'name' => 'Mreža - šverc cigareta',
-                    'code' => 'sverc',
+                222 => [
+                    'name' => 'Full stop',
+                    'code' => 'tacka',
                 ],
-                350 => [
-                    'name' => 'Paravojna formacija Škorpioni',
-                    'code' => 'skorpioni',
+                230 => [
+                    'name' => '(Ne)moć države',
+                    'code' => 'nemocdrzave',
                 ],
-                340 => [
-                    'name' => 'Ubistvo u Višnjićevu',
-                    'code' => 'visnjicevo',
-                ],
-                270 => [
-                    'name' => 'Ubistvo na Ibarskoj magistrali',
-                    'code' => 'ibarska',
-                ],
-                280 => [
-                    'name' => 'Srpska pravda',
-                    'code' => 'pravosudje',
-                ],
-                320 => [
-                    'name' => 'Makina grupa',
-                    'code' => 'mgrupa',
-                ],
-                310 => [
-                    'name' => 'Intervju - Milan Obradović',
-                    'code' => 'mobradovic',
-                ],
-                300 => [
-                    'name' => 'Intervju - Čedomir Jovanović',
-                    'code' => 'cjovanovic',
-                ],
-                330 => [
-                    'name' => 'Ubistvo Zorana Đinđića',
-                    'code' => 'ubistvopremijera',
-                ],
-                290 => [
-                    'name' => 'Intervju - Vladimir Beba Popović',
-                    'code' => 'vbpopovic',
-                ],
-                130 => [
-                    'name' => 'Rukopisi ne gore',
-                    'code' => 'rukopisi',
-                ],
-                250 => [
-                    'name' => 'Tragom Ratka Mladića',
-                    'code' => 'mladic',
-                ],
-                555 => [
-                    'name' => 'Zloupotrebama do Pančićevog vrha',
-                    'code' => 'pancicevvrh',
-                ],
-                700 => [
-                    'name' => 'Izbori 2018',
-                    'code' => 'izbori2018',
-                ],
-                600 => [
-                    'name' => 'Državna pljačka države',
-                    'code' => 'drzavnapljacka',
-                ],
-                560 => [
-                    'name' => 'Insajder na lokalu',
-                    'code' => 'insajderlokal',
-                ],
-                550 => [
-                    'name' => 'Predsednički izbori 2017',
-                    'code' => 'predsednicki2017',
-                ],
-                543 => [
-                    'name' => 'Cena kosovske iluzije',
-                    'code' => 'kosovo',
-                ],
-                542 => [
-                    'name' => '"Posao veka" koji je propao',
-                    'code' => 'juznitok',
-                ],
-                541 => [
-                    'name' => 'Komunalna policija',
-                    'code' => 'komunalci',
-                ],
-                540 => [
-                    'name' => 'DIPOS - Rasipnik državnog novca',
-                    'code' => 'dipos',
-                ],
-                530 => [
-                    'name' => 'Mediji: Rat za istinu?',
-                    'code' => 'ratzaistinu',
-                ],
-                520 => [
-                    'name' => 'Izbeglice na ruti užasa',
-                    'code' => 'izbeglice',
+                405 => [
+                    'name' => 'prodajaeng',
+                    'code' => 'prodaja',
                 ],
                 510 => [
-                    'name' => 'Izbori 2016.',
-                    'code' => 'izbori2016',
+                    'name' => 'Elections 2016',
+                    'code' => '510',
+                ],
+                520 => [
+                    'name' => 'Refugees on horror route',
+                    'code' => 'refugees',
+                ],                
+                530 => [
+                    'name' => 'Media: War for truth',
+                    'code' => 'mediawar',
+                ],
+                540 => [
+                    'name' => 'DIPOS, squanderer of state money',
+                    'code' => 'DIPOS',
                 ],
             ],
         ],
